ccc105dc3b12ce8fe873286d8fcd3c0effdc7131
churi <santiagom@gmail.com>
2021-07-03 12:32:32 -0300

    Creacion proyecto Enigma Billeteras

---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿let menu = document.querySelector('#navBarra');
+
+window.addEventListener('scroll', () => {
+    let scrollSuperior = document.documentElement.scrollTop || window.pageYOffset;
+    if (scrollSuperior !== 0) {
+        menu.style.backgroundColor = 'black';
+        menu.style.position = 'fixed';
+    } else {
+        menu.style.backgroundColor = 'transparent';
+        menu.style.position = 'relative';
+    }
+});
